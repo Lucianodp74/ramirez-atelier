dfe472fd182d1687e6a2e6ac2f339d1c64de4850
Lucianodp74 <lucianodelpriore74@gmail.com>
2026-08-29 15:27:44 +0200

    fix: corregge stringa del test workflow commessa

--- src/server/services/commessa-service.test.ts
+++ src/server/services/commessa-service.test.ts
@@ -13,7 +13,7 @@ describe('workflow commessa', () => {
     expect(transizioneCommessaAmmessa('CONSEGNATA', 'CHIUSA')).toBe(true);
   });
 
-  it('non consente salti all'indietro o chiusure premature', () => {
+  it("non consente salti all'indietro o chiusure premature", () => {
     expect(transizioneCommessaAmmessa('DA_AVVIARE', 'PRONTA')).toBe(false);
     expect(transizioneCommessaAmmessa('IN_PRODUZIONE', 'CONSEGNATA')).toBe(false);
     expect(transizioneCommessaAmmessa('PRONTA', 'IN_PRODUZIONE')).toBe(false);
